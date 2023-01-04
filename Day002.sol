c2f219e63b5522cebca129edfc3d7142008eee3d
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2023-01-04 20:33:34 +0530

    Added Day 002

---
+++ Day002.sol
@@ -0,0 +1,12 @@
+//First Application TutorialsPoint
+pragma solidity ^0.5.0;
+contract SolidityTest {
+   constructor() public{
+   }
+   function getResult() public view returns(uint){
+      uint a = 1;
+      uint b = 2;
+      uint result = a + b;
+      return result;
+   }
+}
