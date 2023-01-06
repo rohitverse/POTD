1e721f6573e8f5a26c946736d5f1c548c4635168
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2023-01-06 21:01:05 +0530

    Added State Variable

---
+++ Day003.sol
@@ -0,0 +1,8 @@
+//StateVariable
+pragma solidity ^0.5.0;
+contract SolidityTest {
+   uint storedData;      // State variable
+   constructor() public {
+      storedData = 10;   // Using State variable
+   }
+}
