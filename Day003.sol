dd1f288405f84731a52543fb3424c308bec565d9
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2023-01-09 14:11:33 +0530

    Delete Day003.sol

--- Day003.sol
+++
@@ -1,8 +0,0 @@
-//StateVariable
-pragma solidity ^0.5.0;
-contract SolidityTest {
-   uint storedData;      // State variable
-   constructor() public {
-      storedData = 10;   // Using State variable
-   }
-}
