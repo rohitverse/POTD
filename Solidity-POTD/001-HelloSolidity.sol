7b4e75c97583cb92acc6ed4bdecf5e51119951d1
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2023-01-11 14:07:55 +0530

    Added Day 01 - Hello World

---
+++ Solidity-POTD/001-HelloSolidity.sol
@@ -0,0 +1,6 @@
+//Task - To complete this challenge, you must print Hello Solidity. Use str as a variable to store value.
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract HelloSolidity{
+ string public str="Hello Solidity";
+}
